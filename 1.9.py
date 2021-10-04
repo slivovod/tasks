ff8a8e9f4b149c8a6eaeca6004144ff63f36ce2e
slivovod <61585943+slivovod@users.noreply.github.com>
2021-10-04 14:11:36 +0300

    Add files via upload

---
+++ 1.9.py
@@ -0,0 +1,11 @@
+first_num = float(input())
+second_num = float(input())
+third_num = float(input())
+
+exp1 = first_num + second_num - third_num
+exp2 = (second_num + third_num) / first_num
+exp3 = first_num * third_num - second_num
+
+print("expression 1: ", exp1)
+print("expression 2: ", exp2)
+print("expression 3: ", exp3)
